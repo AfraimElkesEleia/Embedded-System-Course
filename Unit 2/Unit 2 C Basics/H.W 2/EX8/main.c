8128254af52f74003d52b50ef9915c199a67ca4f
Afraim <afraimeleia200@gmail.com>
2024-03-11 21:11:43 +0200

    correct Ex8 file

--- Unit 2/Unit 2 C Basics/H.W 2/EX8/main.c
+++ Unit 2/Unit 2 C Basics/H.W 2/EX8/main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
-
 int main(){
-
+	char c ;
+	float num1 , num2;
+	printf("Enter two operator either + or - or * or divide : ");
+	fflush(stdin);fflush(stdout);
+	scanf("%c",&c);
+	printf("Enter two operands : ");
+	fflush(stdout);fflush(stdin);
+	scanf("%f%f",&num1,&num2);
+	float result = 0;
+	switch(c){
+	case'+':
+		result = num1 + num2;
+		break;
+	case'-':
+		result = num1 - num2;
+		break;
+	case'*':
+		result = num1 * num2;
+		break;
+	case'/':
+		result = num1/num2;
+		break;
+	default:
+		printf("Error !!! this is not arithmetic operator");
+		return 0;
+	}
+	printf("%0.2f %c %0.2f = %0.2f",num1,c,num2 , result);
+return 0;
 }
